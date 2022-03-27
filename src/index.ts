517912f44bb9f7d79e449d5b6d6f14512d249e5a
Satish Babariya <satish.babariya@gmail.com>
2022-03-27 13:21:41 +0530

    add commander

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
-export function run() {
-  console.log("Hello world!");
+import commander from "commander";
+
+const pkg = require("../package.json");
+
+let program: commander.Command;
+
+async function main() {
+  program = new commander.Command();
+  program.version(`${pkg.version}`);
+
+  program
+    .command("init")
+    .alias("i")
+    .description("Initialize a new project")
+    .action(() => {
+      console.log("Initializing a new project");
+    });
+
+  program.parse(process.argv);
 }
 
-run();
+main();
